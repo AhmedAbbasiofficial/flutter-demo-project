bf4bd0de5a797716bd918ac896650deb7e291e35
AhmedAbbasiofficial <abbasiahmed4433@gmail.com>
2025-03-23 19:43:13 +0500

    create chatting

---
+++ lib/chat/chatting_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class chats extends StatefulWidget {
+  const chats({super.key});
+
+  @override
+  State<chats> createState() => _chatsState();
+}
+
+class _chatsState extends State<chats> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("abbasi"),
+      ),
+    );
+  }
+}
